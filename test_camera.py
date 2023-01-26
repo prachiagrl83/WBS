af72b4e43c9f8fd87cb1856f86e6b4596e893c37
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-26 12:28:36 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -1,16 +1,14 @@
+import cv2
 import streamlit as st
 
-from camera_input_live import camera_input_live
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-def camera_input_live(
-    debounce: int = 1000,
-    height: int = 530,
-    width: int = 704,
-    key: Optional[str] = None,
-    show_controls: bool = True,
-    start_label: str = "Start capturing",
-    stop_label: str = "Pause capturing")
-
-image = camera_input_live()
-
-st.image(value)
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')

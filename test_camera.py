63366a596414ca65a3fbaca70ffd0003f1ee5a9b
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-26 12:23:50 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -2,6 +2,16 @@ import streamlit as st
 
 from camera_input_live import camera_input_live
 
+def camera_input_live(
+    debounce: int = 1000,
+    height: int = 530,
+    width: int = 704,
+    key: Optional[str] = None,
+    show_controls: bool = True,
+    start_label: str = "Start capturing",
+    stop_label: str = "Pause capturing",
+)
+
 image = camera_input_live()
 
 st.image(value)

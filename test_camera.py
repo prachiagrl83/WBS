2ebd6b41ef4e66724b6af057aab69e20ee0470f4
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-26 12:56:00 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.markdown(   f”””   <style>
+   p {
+   background-image: url('fashion.jpg');
+   }
+   </style>   ”””,   unsafe_allow_html=True)
+
 picture = st.camera_input("Take a picture")
 
 if picture:

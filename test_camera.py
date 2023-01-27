530cad98d3b9e2c8cb72683ea7c85e9850d65d7c
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-27 13:00:53 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -19,15 +19,21 @@ picture = st.camera_input("Take a picture")
 
 if picture:
     st.title('Top Similar products')
+    
+    col1, col2, col3 = st.columns(4)
+    with col1:
     image1 = Image.open("./48313.jpg")
     new1 = image1.resize((600, 400))
     st.image(new1)
+    with col2:
     image2 = Image.open("./48318.jpg")
     new2 = image2.resize((600, 400))
     st.image(new2)
+    with col3:
     image3 = Image.open("./48319.jpg")
     new3 = image3.resize((600, 400))
     st.image(new3)
+    with col4:
     image4 = Image.open("./48320.jpg")
     new4 = image4.resize((600, 400))
     st.image(new4)

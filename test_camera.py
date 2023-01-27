9af925431c7792d232430ad68198959454b596cb
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-27 08:09:48 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 picture = st.camera_input("Take a picture")
 
 if picture:

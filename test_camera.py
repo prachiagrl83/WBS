7b586fc5f2e035243e9c6e2303d7357fb56c6f76
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-27 17:47:13 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -11,8 +11,8 @@ background-size: cover;
 }
 </style>
 """
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
+st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title(':red[_FashCam_] :sunglasses:')
 
 #st.image('./fashion.jpg')
@@ -21,33 +21,20 @@ picture = st.camera_input("Take a picture")
 if picture:
     st.title('Top Similar products') 
     st.balloons()
-    # Now display four more images
-    row = []
-    for i in range(1, 4):
-        img = Image.open(f"image{i}.jpg")
-        row.append(img)
-        else:
-        st.warning("Please take a picture again.")
-    
     #col1, col2, col3 = st.columns(4)
     #with col1:
-    #image1 = Image.open("./48313.jpg")
-    #new1 = image1.resize((200, 200))    
-    #st.image(new1)
+    image1 = Image.open("./48313.jpg")
+    new1 = image1.resize((500, 300))    
+    st.image(new1)
     #with col2:
-    #image2 = Image.open("./48318.jpg")
-    #new2 = image2.resize((200, 200))    
-    #st.image(new2)
+    image2 = Image.open("./48318.jpg")
+    new2 = image2.resize((500, 300))    
+    st.image(new2)
     #with col3:
-    #image3 = Image.open("./48319.jpg")
-    #new3 = image3.resize((200, 200))    
-    #st.image(new3)
+    image3 = Image.open("./48319.jpg")
+    new3 = image3.resize((500, 300))    
+    st.image(new3)
     #with col4:
-    #image4 = Image.open("./48320.jpg")
-    #new4 = image4.resize((200, 200))    
-    #st.image(new4)
-    
-
-    
-    
-    
+    image4 = Image.open("./48320.jpg")
+    new4 = image4.resize((500, 300))    
+    st.image(new4)

64f71d144c449bc2a4b1e301e1327ac58c9abb32
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-27 08:59:47 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 page_by_img = """
 <style>
-[data-testid-"st.AppViewContainer"]{
+[data-testid-"stAppViewContainer"]{
 background-image: url("https://unsplash.com/photos/nimElTcTNyY):
 background-size: cover;
 }
 </style>
 """
 
-st.markdown(page_bg_img,unsafe_allow_html=True)
+st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title('FashCam')
 
 #st.image('./fashion.jpg')

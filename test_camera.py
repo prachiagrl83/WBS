658ddc48b2fd3a850df4a65ce1cbd5e7b0de9dd0
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-30 18:53:53 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -5,8 +5,7 @@ st.set_page_config(page_title="Image Uploader",page_icon=":camera:", layout="wid
 
 col1,mid,col2 = st.columns([1,10,100])
 with col1:
-    image = Image.open('Fashion_Camera.jpg')
-    image.resize((300,300))
+    st.image('Fashion_Camera.jpg', width=300)
 with col2:
     #st.write('A Name')
     st.markdown('<h1 style="color: red;">FashCam</h1>',

1bc58abd21daafdebd50ca525e7bc11fc41317fe
Prachi Agrawal <113503622+prachiagrl83@users.noreply.github.com>
2023-01-30 20:16:15 +0100

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -8,9 +8,9 @@ with col1:
     st.image('Fashion_Camera1.jpg', width=150)
 with col2:
     #st.write('A Name')
-    st.markdown('<h1 style="color: red;font-size: 70px;">FashCam \n ...an Image Search Engine</h1>',
+    st.markdown('<h1 style="color: red;font-size: 70px;">FashCam</h1>',
                             unsafe_allow_html=True)
-    st.markdown('<h1 style="color: black;font-size: 30px;">...a Image Search Engine</h1>',
+    st.markdown('<h1 style="color: black;font-size: 30px;">...an Image Search Engine</h1>',
                             unsafe_allow_html=True)
     
     st.markdown("Our idea is to build a new search engine **:red[_FashCam_]**:camera:. We know that online shopping can be frustrating, especially when you're trying to find fashion products that are similar to ones you've seen in real life. That's why we have developed a cutting-edge image recognition technology that makes it easy to find the fashion you want.") 
